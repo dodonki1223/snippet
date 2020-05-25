afea31f53be5843a8b051ababd9d91955204de0c
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-25 23:54:22 +0900

    :sparkles: 1-1 Rails Scaffold を利用して爆速で CRUD機能を実現しよう - DB migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_25_142123) do
+
+  create_table "snippets", force: :cascade do |t|
+    t.string "title"
+    t.string "language"
+    t.string "contents"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
