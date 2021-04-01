2e5471f0c386fabf7d6cd29a9ffd0e21e3afa527
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-01 11:15:10 +0900

    :wrench: Rubocop の新バージョン対応

--- spec/requests/snippets_spec.rb
+++ spec/requests/snippets_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Snippets', type: :request do
       # テストを実行する時は RAILS_ENV=test で実行すること
       # ref:https://qiita.com/taiteam/items/a37c60fc15c1aa5bb606#3-2-%E3%83%87%E3%83%95%E3%82%A9%E3%83%AB%E3%83%88%E8%A8%AD%E5%AE%9A
       get snippets_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
